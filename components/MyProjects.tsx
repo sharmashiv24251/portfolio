1f171d57aa97fa89787ced0a6c3b73e5f4f6ee31
sharmashiv24251 <sharmashiv24251@gmail.com>
2024-11-10 01:37:04 +0530

    fix: deployment errors

--- components/MyProjects.tsx
+++ components/MyProjects.tsx
@@ -105,7 +105,7 @@ export default function MyProjects() {
                       <span className="absolute inset-[-1000%] animate-[spin_2s_linear_infinite] bg-[conic-gradient(from_90deg_at_50%_50%,#E2CBFF_0%,#393BB2_50%,#E2CBFF_100%)]" />
                       <span className="inline-flex h-full w-full cursor-pointer items-center justify-center rounded-full bg-slate-950 px-3 py-1 text-sm font-medium text-white backdrop-blur-3xl">
                         Visit
-                      </span>
+                      </span>{" "}
                     </motion.a>
                     <motion.a
                       layout
@@ -207,11 +207,11 @@ export const CloseIcon = () => {
 };
 
 const cards: {
-  description: String;
-  title: String;
-  src: String;
-  visitUrl?: String;
-  githubUrl?: String;
+  description: string;
+  title: string;
+  src: string;
+  visitUrl?: string;
+  githubUrl?: string;
   content: any;
 }[] = [
   {
