68f469e7b4bb0612758b533094a14fe1ef41b7aa
sharmashiv24251 <sharmashiv24251@gmail.com>
2024-11-10 01:30:42 +0530

    chore : add project links

--- components/MyProjects.tsx
+++ components/MyProjects.tsx
@@ -3,6 +3,7 @@ import Image from "next/image";
 import React, { useEffect, useId, useRef, useState } from "react";
 import { AnimatePresence, motion } from "framer-motion";
 import { useOutsideClick } from "@/hooks/use-outside-click";
+import { SiGithub } from "react-icons/si";
 
 export default function MyProjects() {
   const [active, setActive] = useState<(typeof cards)[number] | boolean | null>(
@@ -38,27 +39,21 @@ export default function MyProjects() {
             initial={{ opacity: 0 }}
             animate={{ opacity: 1 }}
             exit={{ opacity: 0 }}
-            className="fixed  bg-black/20 h-full w-full z-10"
+            className="fixed bg-black/20 h-full w-full z-10"
           />
         )}
       </AnimatePresence>
       <AnimatePresence>
         {active && typeof active === "object" ? (
-          <div className="fixed inset-0  grid place-items-center z-[100]">
+          <div className="fixed inset-0 grid place-items-center z-[100]">
             <motion.button
               key={`button-${active.title}-${id}`}
               layout
-              initial={{
-                opacity: 0,
-              }}
-              animate={{
-                opacity: 1,
-              }}
+              initial={{ opacity: 0 }}
+              animate={{ opacity: 1 }}
               exit={{
                 opacity: 0,
-                transition: {
-                  duration: 0.05,
-                },
+                transition: { duration: 0.05 },
               }}
               className="flex absolute top-2 right-2 lg:hidden items-center justify-center bg-white rounded-full h-6 w-6"
               onClick={() => setActive(null)}
@@ -68,11 +63,10 @@ export default function MyProjects() {
             <motion.div
               layoutId={`card-${active.title}-${id}`}
               ref={ref}
-              className="w-full max-w-[500px]  h-full md:h-fit md:max-h-[90%]  flex flex-col bg-white dark:bg-neutral-900 sm:rounded-3xl overflow-hidden"
+              className="w-full max-w-[500px] h-full md:h-fit md:max-h-[90%] flex flex-col bg-white dark:bg-neutral-900 sm:rounded-3xl overflow-hidden"
             >
               <motion.div layoutId={`image-${active.title}-${id}`}>
                 <Image
-                  priority
                   width={300}
                   height={300}
                   src={active.src}
@@ -83,7 +77,7 @@ export default function MyProjects() {
 
               <div>
                 <div className="flex justify-between items-start p-4">
-                  <div className="">
+                  <div>
                     <motion.h3
                       layoutId={`title-${active.title}-${id}`}
                       className="font-medium text-neutral-700 dark:text-neutral-200 text-base"
@@ -98,17 +92,34 @@ export default function MyProjects() {
                     </motion.p>
                   </div>
 
-                  <motion.a
-                    layout
-                    initial={{ opacity: 0 }}
-                    animate={{ opacity: 1 }}
-                    exit={{ opacity: 0 }}
-                    href={active.ctaLink}
-                    target="_blank"
-                    className="px-4 py-3 text-sm rounded-full font-bold bg-green-500 text-white"
-                  >
-                    {active.ctaText}
-                  </motion.a>
+                  <div className="flex gap-2">
+                    <motion.a
+                      layout
+                      initial={{ opacity: 0 }}
+                      animate={{ opacity: 1 }}
+                      exit={{ opacity: 0 }}
+                      href={active.visitUrl}
+                      target="_blank"
+                      className="relative inline-flex h-12 overflow-hidden rounded-full p-[1px] focus:outline-none focus:ring-2 focus:ring-slate-400 focus:ring-offset-2 focus:ring-offset-slate-50"
+                    >
+                      <span className="absolute inset-[-1000%] animate-[spin_2s_linear_infinite] bg-[conic-gradient(from_90deg_at_50%_50%,#E2CBFF_0%,#393BB2_50%,#E2CBFF_100%)]" />
+                      <span className="inline-flex h-full w-full cursor-pointer items-center justify-center rounded-full bg-slate-950 px-3 py-1 text-sm font-medium text-white backdrop-blur-3xl">
+                        Visit
+                      </span>
+                    </motion.a>
+                    <motion.a
+                      layout
+                      initial={{ opacity: 0 }}
+                      animate={{ opacity: 1 }}
+                      exit={{ opacity: 0 }}
+                      href={active.githubUrl}
+                      target="_blank"
+                      className="px-4 py-2 text-sm rounded-full font-bold  bg-green-500 text-white hover:bg-green-600 transition-colors flex items-center gap-2"
+                    >
+                      <SiGithub className="h-4 w-4" />
+                      GitHub
+                    </motion.a>
+                  </div>
                 </div>
                 <div className="pt-4 relative px-4">
                   <motion.div
@@ -129,12 +140,12 @@ export default function MyProjects() {
         ) : null}
       </AnimatePresence>
       <ul className="w-full mt-4 grid max-sm:grid-cols-1 grid-cols-2 md:grid-cols-3 lg:grid-cols-4 items-center gap-4">
-        {cards.map((card, index) => (
+        {cards.map((card) => (
           <motion.div
             layoutId={`card-${card.title}-${id}`}
             key={card.title}
             onClick={() => setActive(card)}
-            className="p-4 flex flex-col  hover:bg-neutral-50 bg-neutral-900 dark:hover:bg-neutral-800 rounded-xl cursor-pointer"
+            className="p-4 flex flex-col hover:bg-neutral-50 bg-neutral-900 dark:hover:bg-neutral-800 rounded-xl cursor-pointer"
           >
             <div className="flex gap-4 flex-col w-full">
               <motion.div layoutId={`image-${card.title}-${id}`}>
@@ -143,7 +154,7 @@ export default function MyProjects() {
                   height={300}
                   src={card.src}
                   alt={card.title}
-                  className="h-60 w-full  rounded-lg object-cover object-top"
+                  className="h-60 w-full rounded-lg object-cover object-top"
                 />
               </motion.div>
               <div className="flex justify-center items-start flex-col">
@@ -171,17 +182,11 @@ export default function MyProjects() {
 export const CloseIcon = () => {
   return (
     <motion.svg
-      initial={{
-        opacity: 0,
-      }}
-      animate={{
-        opacity: 1,
-      }}
+      initial={{ opacity: 0 }}
+      animate={{ opacity: 1 }}
       exit={{
         opacity: 0,
-        transition: {
-          duration: 0.05,
-        },
+        transition: { duration: 0.05 },
       }}
       xmlns="http://www.w3.org/2000/svg"
       width="24"
@@ -201,13 +206,20 @@ export const CloseIcon = () => {
   );
 };
 
-const cards = [
+const cards: {
+  description: String;
+  title: String;
+  src: String;
+  visitUrl?: String;
+  githubUrl?: String;
+  content: any;
+}[] = [
   {
     description: "airbnb clone ",
     title: "Blue-BnB",
     src: "https://opfjwckyarxymdkzuwdk.supabase.co/storage/v1/object/public/temp-blue-bnb/bnb.png",
-    ctaText: "Visit",
-    ctaLink: "https://ui.aceternity.com/templates",
+    githubUrl: "https://github.com/sharmashiv24251/blue-bnb",
+    visitUrl: "https://blue-bnb.vercel.app/",
     content: () => {
       return <p>hii</p>;
     },
@@ -216,8 +228,8 @@ const cards = [
     description: "free certificates",
     title: "Certificate app",
     src: "https://opfjwckyarxymdkzuwdk.supabase.co/storage/v1/object/public/temp-blue-bnb/certificate.png",
-    ctaText: "Visit",
-    ctaLink: "https://ui.aceternity.com/templates",
+    visitUrl: "https://build-it-sigma.vercel.app/",
+    githubUrl: "https://github.com/sharmashiv24251/build-it",
     content: () => {
       return <p>yooo</p>;
     },
@@ -226,8 +238,8 @@ const cards = [
     description: "Filterable Charts from Api",
     title: "Chart App",
     src: "https://opfjwckyarxymdkzuwdk.supabase.co/storage/v1/object/public/temp-blue-bnb/chart.png?t=2024-11-09T19%3A27%3A47.459Z",
-    ctaText: "Visit",
-    ctaLink: "https://ui.aceternity.com/templates",
+    visitUrl: "https://rock8-chart.vercel.app/",
+    githubUrl: "https://github.com/sharmashiv24251/rock8_chart",
     content: () => {
       return <p>chart ap</p>;
     },
@@ -237,8 +249,8 @@ const cards = [
     description: "fetch emails and email body from api",
     title: "Outlook clone",
     src: "https://opfjwckyarxymdkzuwdk.supabase.co/storage/v1/object/public/temp-blue-bnb/outlook.png",
-    ctaText: "Visit",
-    ctaLink: "https://ui.aceternity.com/templates",
+    githubUrl: "https://github.com/sharmashiv24251/Outlook_rock8",
+    visitUrl: "https://outlook-rock8.vercel.app/",
     content: () => {
       return (
         <p>
@@ -258,9 +270,9 @@ const cards = [
   {
     description: "Html css and js only",
     title: "Old Portfolio",
+    visitUrl: "https://silver-tanuki-98f89f.netlify.app/",
+    githubUrl: "https://github.com/sharmashiv24251",
     src: "https://opfjwckyarxymdkzuwdk.supabase.co/storage/v1/object/public/temp-blue-bnb/portfolio.png?t=2024-11-09T19%3A30%3A32.048Z",
-    ctaText: "Visit",
-    ctaLink: "https://ui.aceternity.com/templates",
     content: () => {
       return <p>mre disjsd d s</p>;
     },
